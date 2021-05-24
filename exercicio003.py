4b77366c716b5fb7dd81f3bfe8e84fd3ef135889
Larissa Midori <70383649+LarissaMidori@users.noreply.github.com>
2021-05-24 11:30:10 -0300

    Adicionando exercício 3
    Crie um programa que leia 2 números e mostre a soma entre eles.
    
    Crie um programa que leia 2 números e mostre a multiplicação entre eles.

---
+++ exercicio003.py
@@ -0,0 +1,16 @@
+# Crie um programa que leia 2 números e mostre a soma entre eles
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+print(f'A soma entre {n1} e {n2} é: {s}')
+#print(type(n1)) - mostra o tipo do dado recebido
+
+
+# Crie um programa que leia 2 números e mostre a multiplicação entre eles
+
+n3 = int(input('Digite um valor: '))
+n4 = int(input('Digite outro valor: '))
+m = n3 * n4
+print(f'A multiplicação entre {n3} e {n4} é: {m}')
+
