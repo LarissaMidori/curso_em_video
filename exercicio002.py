2c7050e90fd9541f6c30b495c6e54fd404ec7178
Larissa Midori <70383649+LarissaMidori@users.noreply.github.com>
2021-05-24 11:34:18 -0300

    Update exercicio002.py

--- exercicio002.py
+++ exercicio002.py
@@ -1,4 +1,5 @@
 # Faça um programa que mostre o nome da pessoa e mostre uma mensagem de boas-vindas
 
 nome = input('Digite seu nome: ')
-print('É um prazer em te conhecer, {}!'.format(nome))
+#print('É um prazer em te conhecer, {}!'.format(nome))
+print(f'É um prazer em te conhecer, {nome}!') 
