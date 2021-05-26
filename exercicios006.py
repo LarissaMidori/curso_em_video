1080b3313013064c7e909681b6a05706b13133a7
Larissa Midori <70383649+LarissaMidori@users.noreply.github.com>
2021-05-26 20:30:45 -0300

    Criando exercicio006.py
    Crie um algoritmo que leia um número e mostre o seu dobro, triplo e a raiz quadrada

---
+++ exercicios006.py
@@ -0,0 +1,5 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e a raiz quadrada
+
+n = int(input('Digite um número: '))
+
+print(f'O número {n} tem como dobro {n*2}, triplo {n*3} e raiz quadrada {n**(1/2):.2f}')
