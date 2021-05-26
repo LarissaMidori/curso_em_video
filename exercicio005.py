1ea31255f1749abcbdcd201d34a9aaed5c0d0810
Larissa Midori <70383649+LarissaMidori@users.noreply.github.com>
2021-05-26 12:40:37 -0300

    Criando exercicio005.py
    Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e o seu antecessor

---
+++ exercicio005.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e o seu antecessor
+
+n = int(input('Digite um número: '))
+print(f'Analisando o número {n}, o antecessor dele é {n - 1} e o sucessor é {n + 1}')
