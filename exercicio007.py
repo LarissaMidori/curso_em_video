f0466b6c629d5d8e244be82311e6cd6b04098823
Larissa Midori <70383649+LarissaMidori@users.noreply.github.com>
2021-05-26 23:11:27 -0300

    Criando exercicio007.py
    Desenvolva um programa que leia as 2 notas de um aluno, calcule e mostre sua média

---
+++ exercicio007.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que leia as 2 notas de um aluno, calcule e mostre sua média
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+
+print(f'A soma entre as notas {nota1} e {nota2} é: {(nota1 + nota2):.1f} e a média é: {media:.1f}')
+
