625bbc599852538c7dad126285bfdfd59a2d65bd
Larissa Midori <70383649+LarissaMidori@users.noreply.github.com>
2021-05-30 23:39:17 -0300

    Update exercicio032.py

--- exercicio032.py
+++ exercicio032.py
@@ -8,4 +8,5 @@ if ano == 0:
 if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0: # (divisível por 4, mas não por 100) ou divisível por 400
     print(f'O ano {ano} é BISSEXTO.')
 else:
-    print(f'O ano {ano} NÃO é BISSEXTO')
+    print(f'O ano {ano} NÃO é BISSEXTO.')
+    
