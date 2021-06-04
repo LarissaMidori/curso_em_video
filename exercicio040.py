79666e04af421452e8a34b9228b0c4409b0be276
Larissa Midori <70383649+LarissaMidori@users.noreply.github.com>
2021-06-04 00:52:05 -0300

    Criando exercicio040.py
    Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
    
    – Média abaixo de 5.0: REPROVADO
    – Média entre 5.0 e 6.9: RECUPERAÇÃO
    – Média 7.0 ou superior: APROVADO

---
+++ exercicio040.py
@@ -0,0 +1,16 @@
+''' Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
+
+– Média abaixo de 5.0: REPROVADO
+– Média entre 5.0 e 6.9: RECUPERAÇÃO
+– Média 7.0 ou superior: APROVADO '''
+
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda nota: '))
+media = (nota1 + nota2) / 2
+if media < 5.0:
+    print(f'Com as notas {nota1} e {nota2}, a média foi {media:.1f}. E o aluno foi REPROVADO!')
+elif media >= 5.0 and media < 7:
+    print(f'Com as notas {nota1} e {nota2}, a média foi {media:.1f}. E o aluno está em RECUPERAÇÃO.')
+elif media >= 7.0:
+    print(f'Com as notas {nota1} e {nota2}, a média foi {media:.1f}. E aluno foi APROVADO!')
+    
